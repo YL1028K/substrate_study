616f4314019ea4072cd7eda6caa58b6712fd9e26
YL1028K <373218473@qq.com>
2023-11-08 18:12:27 +0800

    add week3: bubble sort with generic

---
+++ substrate_week3/src/main.rs
@@ -0,0 +1,77 @@
+use std::cmp::Ordering;
+
+fn main() {
+    // println!("Hello, world!");
+}
+
+fn bubble_sort_i32(nums: &mut [i32]) {
+    if nums.len() < 2 {
+        return;
+    }
+    let mut i: usize = 0;
+    while i < nums.len() {
+        let mut j: usize = 1;
+        while j < nums.len() {
+            if nums[j] < nums[j - 1] {
+                nums.swap(j, j - 1);
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+    return;
+}
+
+fn bubble_sort<T: std::cmp::PartialOrd>(nums: &mut [T]) {
+    if nums.len() < 2 {
+        return;
+    }
+    let mut i: usize = 0;
+    while i < nums.len() {
+        let mut j: usize = 1;
+        while j < nums.len() {
+            match nums[j].partial_cmp(&nums[j - 1]) {
+                Some(Ordering::Less) => nums.swap(j, j - 1),
+                _ => (),
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+    return;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_sort_i32() {
+        let mut nums: [i32; 6] = [-10, 2, 20, -3, 1, 10];
+        let sorted = [-10, -3, 1, 2, 10, 20];
+        bubble_sort_i32(&mut nums);
+        assert_eq!(nums, sorted);
+    }
+
+    #[test]
+    fn test_sort() {
+        let mut nums: [i32; 6] = [-10, 2, 20, -3, 1, 10];
+        let sorted: [i32; 6] = [-10, -3, 1, 2, 10, 20];
+        bubble_sort(&mut nums);
+        assert_eq!(nums, sorted);
+
+        let mut strs: [String; 4] = [
+            String::from("d"),
+            String::from("z"),
+            String::from("c"),
+            String::from("q"),
+        ];
+        let sorted_strs: [String; 4] = [
+            String::from("c"),
+            String::from("d"),
+            String::from("q"),
+            String::from("z"),
+        ];
+        bubble_sort(&mut strs);
+        assert_eq!(strs, sorted_strs);
+    }
+}
